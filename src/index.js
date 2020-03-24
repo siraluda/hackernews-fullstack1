c368767011092b199fd855345514dae73480cd97
abdul_waris <siraluda2020@gmail.com>
2020-03-24 21:19:55 -0230

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
+
+// 1 importing the required dependencies from the installed packages (apollo-boost, react-apollo, graphql)
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from "apollo-cache-inmemory"
+
+// 2  create the httpLink that will connect your ApolloClient instance with the GraphQL API, your GraphQL server will be running on http://localhost:4000
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+//3 instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
+//4 render the root component of your React app. The App is wrapped with the higher-order component ApolloProvider that gets passed the client as a prop
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ApolloProvider client={ client }>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
